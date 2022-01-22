ddc67b7a38ea7b1da4ad0825dceb11c7566dce19
Chaman jain <chamanjain199@gmail.com>
2022-01-22 10:15:11 +0530

    sql limiting

---
+++ 13-sales-limiting.sql
@@ -0,0 +1,7 @@
+-- SELECT * FROM sales order by volume;
+-- SELECT * FROM sales order by volume desc;
+
+-- SELECT * FROM sales order by volume LIMIT 5;
+-- SELECT * FROM sales order by volume LIMIT 5 OFFSET 2;
+
+-- SELECT DISTINCT(customer_name) from sales order by customer_name;
