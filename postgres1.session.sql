3197223383c6a7372ba6c8a4cb9cf1bceed07058
Chaman jain <chamanjain199@gmail.com>
2022-02-12 23:18:28 +0530

    grouping by

--- postgres1.session.sql
+++ postgres1.session.sql
@@ -1,14 +1,35 @@
--- CREATE TABLE table1(value1 VARCHAR(200),value2 VARCHAR(200));
--- CREATE TABLE table2(value3 VARCHAR(200),value4 VARCHAR(200));
+-- -- CREATE TABLE table1(value1 VARCHAR(200),value2 VARCHAR(200));
+-- -- CREATE TABLE table2(value3 VARCHAR(200),value4 VARCHAR(200));
 
-INSERT INTO table2 VALUES('1','11');
+-- INSERT INTO table2 VALUES('1','11');
 
-INSERT INTO table2 VALUES('5','11');
-INSERT INTO table2 VALUES('3','33');
-INSERT INTO table2 VALUES('4','44');
+-- INSERT INTO table2 VALUES('5','11');
+-- INSERT INTO table2 VALUES('3','33');
+-- INSERT INTO table2 VALUES('4','44');
 
-INSERT INTO table1 VALUES('1','1');
-INSERT INTO table1 VALUES('2','2');
+-- INSERT INTO table1 VALUES('1','1');
+-- INSERT INTO table1 VALUES('2','2');
 
-INSERT INTO table1 VALUES('3','3');
-INSERT INTO table1 VALUES('4','4');
+-- INSERT INTO table1 VALUES('3','3');
+-- INSERT INTO table1 VALUES('4','4');
+
+INSERT INTO public."FriendsData"
+      VALUES (1,	10,	11,	FALSE,	1),
+      VALUES (2,	11,	5,	FALSE,	1),
+      VALUES (3,	10,	11,	FALSE,	1),
+      VALUES (4,	5,	25,	FALSE,	1),
+      VALUES (5,	10,	11,	FALSE,	1),
+      VALUES (6,	12,	11,	FALSE,	1),
+VALUES (7,	11,	5,	FALSE,	1),
+VALUES (8,	10,	11,	FALSE,	1),
+VALUES (9,	12,	11,	FALSE,	1);
+
+INSERT INTO public."FriendsData"
+      VALUES (2,	11,	5,	FALSE,	1),
+       (3,	10,	11,	FALSE,	1),
+       (4,	5,	25,	FALSE,	1),
+      (5,	10,	11,	FALSE,	1),
+       (6,	12,	11,	FALSE,	1),
+ (7,	11,	5,	FALSE,	1),
+ (8,	10,	11,	FALSE,	1),
+ (9,	12,	11,	FALSE,	1);
